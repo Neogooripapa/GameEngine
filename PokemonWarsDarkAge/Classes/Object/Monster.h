00571f1ab4263215a975e4d6c143ff069bf99688
조영훈 <jossigger@hanmail.net>
2016-08-03 23:08:13 +0900

    몬스터 클래스 추가

---
+++ PokemonWarsDarkAge/Classes/Object/Monster.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+class Monster
+{
+private:
+	int level;				// 레벨
+	std::string name;		// 이름
+	int hitPoint;			// 체력
+	int skillPoint;			// 기력
+	int physicsAttack;		// 물리 공격력
+	int physicsDefense;		// 물리 방어력
+	int skillDefense;		// 스킬 방어력
+	int skillAttack;		// 스킬 공격력
+	// TODO : 스킬 리스트 저장 변수 추가
+
+public:
+	Monster();
+	~Monster();
+};
